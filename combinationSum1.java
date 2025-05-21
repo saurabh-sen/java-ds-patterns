8ed4421df8ea91541d59b7f601a9838391408714
saurabh-sen <sensaurabh1212@gmail.com>
2025-05-21 07:59:40 +0530

    combination sum pattern

--- combinationSum1.java
+++ combinationSum1.java
@@ -1,7 +1,44 @@
 import java.util.*;
 
 class CombinationSum1 {
-    public static void main(String[] args){
-        System.out.println("hello World!");
+
+    private static void print(List<Integer> ds) {
+        for (int item : ds) {
+            System.err.print(item+" ");
+        }
+        System.err.println();
+    }
+
+    private static void combinationSum1(int i, List<Integer> ds, int[] arr, int target) {
+        // BASE CASE
+        if (i >= arr.length) {
+            if (target == 0) {
+                print(ds);
+            }
+            return;
+        }
+        if(target < 0) {
+            return;
+        }
+
+        // RECURSIVE CALLS
+        // TRICK::: PICK ITEM AND DON'T INCREASE INDEX
+        ds.add(arr[i]);
+        combinationSum1(i, ds, arr, target - arr[i]);
+
+        // NOT PICK ITEM AND INCREASE THE INDEX
+        ds.remove(ds.size() - 1);
+        combinationSum1(i + 1, ds, arr, target);
+    }
+
+    private static void solve(int[] arr, int target) {
+        List<Integer> ds = new ArrayList<>();
+        combinationSum1(0, ds, arr, target);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 7 };
+        int target = 7;
+        solve(arr, target);
     }
 }
